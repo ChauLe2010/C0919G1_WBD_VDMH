84ee9fb6c1692db52e8fa31a33482063c31f5ec4
SpringMike <minhnhatvu2002@gmail.com>
2019-11-11 10:01:58 +0700

    add

--- VNM/08-11-2019/MovePoint/src/MovePoint.java
+++ VNM/08-11-2019/MovePoint/src/MovePoint.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+public class MovePoint extends Point {
+    public float xspeed =0.0f;
+    public float yspeed =0.0f;
 
-public class MovePoint {
+    public MovePoint(float x, float y, float xspeed, float yspeed) {
+        super(x, y);
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+    }
+
+    public MovePoint(float xspeed, float yspeed) {
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+    }
+    public MovePoint(){};
+
+    public float getXspeed() {
+        return xspeed;
+    }
+
+    public void setXspeed(float xspeed) {
+        this.xspeed = xspeed;
+    }
+
+    public float getYspeed() {
+        return yspeed;
+    }
+
+    public void setYspeed(float yspeed) {
+        this.yspeed = yspeed;
+    }
+    public void setSpeed(float xspeed, float yspeed) {
+        this.xspeed = xspeed;
+        this.yspeed = yspeed;
+    }
+    public float[] getSpeed(float xspeed, float yspeed ){
+        float[] xyspeed = {this.xspeed, this.yspeed};
+        return xyspeed;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", speed=" + "( " + xspeed + " , " + yspeed + " )";
+    }
+    public MovePoint move() {
+        setX(getX() + xspeed);
+        setY(getY() + yspeed);
+        return this;
+    }
+
+    public static void main(String[] args) {
+        MovePoint moveablePoint = new MovePoint(2.0f, 4.0f, 5.0f, 5.0f);
+        System.out.println(moveablePoint.move());
+    }
 }
--- VNM/08-11-2019/MovePoint/src/Point.java
+++ VNM/08-11-2019/MovePoint/src/Point.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class Point {
+    public float x = 0.0f;
+    public float y = 0.0f;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Point(){
+
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public float[] getXY(float x, float y ){
+        float[] xy = {this.x, this.y};
+        return xy;
+    }
+    @Override
+    public String toString() {
+        return "{" + "x=" + x + ", y=" + y + '}';
+    }
+
+        public static void main(String[] args) {
+            Point point = new Point(2.0f, 4.0f);
+            String chuoi = point.toString();
+            System.out.println(chuoi);
+        }
+
 }
