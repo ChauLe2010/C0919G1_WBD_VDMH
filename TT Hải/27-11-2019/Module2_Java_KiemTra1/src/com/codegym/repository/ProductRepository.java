ad741095a97bdab09379742d47cb0c6dd84a8b0b
hailita1 <hailit2306@gmail.com>
2019-12-09 08:33:44 +0700

    @@

--- TT Hải/27-11-2019/Module2_Java_KiemTra1/src/com/codegym/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.model.Product;
-
-import java.util.List;
-
-public interface ProductRepository extends GeneralRepository<Product> {
-    List<Product> sortPrice();
-}
--- TT Hải/27-11-2019/Module2_Java_KiemTra1/src/com/codegym/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.service;
-
-import com.codegym.model.Product;
-
-import java.util.List;
-
-public interface ProductService extends GeneralService<Product> {
-    List<Product> sortPrice();
-}
